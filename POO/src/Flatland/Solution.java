5db9ea72df7d7e993af676a9dc81e7476c15988c
FreddyWhitDoubleD <ferreirafreddy39@gmail.com>
2021-10-27 10:53:06 -0300

    prueba tecnica java

---
+++ POO/src/Flatland/Solution.java
@@ -0,0 +1,52 @@
+package Flatland;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the flatlandSpaceStations function below.
+    static int flatlandSpaceStations(int n, int[] c) {
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= c.length; j++) {
+                System.out.println(" + c= " +  c[j]);
+            }
+        }
+
+        return 0;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[] c = new int[m];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = flatlandSpaceStations(n, c);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
