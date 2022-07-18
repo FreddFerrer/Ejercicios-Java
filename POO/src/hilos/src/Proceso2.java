bb94da9b05c6a227b7ae3d0deaf9b97d71f0da76
Freddy Ferreira <74003950+FreddFerrer@users.noreply.github.com>
2022-07-18 12:19:55 -0300

    Delete POO/src/hilos/src directory

--- POO/src/hilos/src/Proceso2.java
+++
@@ -1,8 +0,0 @@
-public class Proceso2 implements Runnable {
-    @Override
-    public void run() {
-        for (int i = 0; i <= 5; i++) {
-            System.out.println("Proceso 2");
-        }
-    }
-}
